b4c033d8493e65b502617148d55b026509914970
michaelseptiadi <septiadimichael@gmail.com>
2024-12-17 18:25:34 +0700

    check media

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import ControlledCarousel from './components/Carousel';
 import ParticlesComponent from './components/Particles';
 
 function App() {
+  const [media, setMedia] = useState(false)
+  useEffect(() =>{
+    setMedia(window.matchMedia("(min-width: 900px)").matches)
+  }, [])
   return (
     <div className="App">
-      <ParticlesComponent/>
-      <ControlledCarousel />
+      <div className="isi">
+        <ParticlesComponent />
+        {
+          media ? <ControlledCarousel /> :
+          <div className="carousel-container">
+            <div className="text-container">
+                <h3 style={{ fontFamily: "Mountains of Christmas", fontWeight: '700', fontSize: '64px' }}>YAHHH GABISA DIBUKA<br/> COBA BUKA DI LAPTOP PASTI BISA DEHHH</h3>
+            </div>
+          </div>
+        }
+      </div>
     </div>
   );
 }
